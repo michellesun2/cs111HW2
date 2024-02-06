0e2862c6562e4c4b4d9c372706a7c2f62a4bb630
michellesun2 <122694710+michellesun2@users.noreply.github.com>
2024-02-05 17:08:38 -0800

    1

--- RSVCode.cpp
+++ RSVCode.cpp
@@ -1,1 +1,65 @@
+//(i) Take three integers, e, n (the public key for RSA), and m (the number of characters in the message) as
+//input to your program. Next, input the ciphertext.
+//(ii) Test whether the public key is valid. If not, output a single line “Public key is not valid!” and quit the
+//program.
+//(iv) If the public key is valid, decode the message.
+//(v) Output p and q, φ(n) and d.
+//(vi) On a new line, output the decoded message in integers.
+//(vii) On a new line, output the decoded message in English. The characters should be all uppercase. You
+//can assume that the numbers will be assigned to characters according to the mapping above.
+//More information and specifications will be provided separately
 
+
+#include <iostream>
+
+using namespace std;
+
+int e;
+int n;
+int m;
+
+vector<int>cipherText;
+   
+    
+cipherText.append(1400); cipherText.append(2218); cipherText.append(99);
+cipherText.append(2088); cipherText.append(4191); cipherText.append(84);
+cipherText.append(843); cipherText.append(99); cipherText.append(4191);
+cipherText.append(3780); cipherText.append(764); cipherText.append(4191);
+cipherText.append(2979); cipherText.append(2269); cipherText.append(99);
+cipherText.append(764); cipherText.append(2218); cipherText.append(2269); cipherText.append(2088);
+cipherText.append(843); cipherText.append(3015); cipherText.append(99); cipherText.append(2970);
+cipherText.append(1443); cipherText.append(1655); cipherText.append(99); cipherText.append(3237);
+cipherText.append(2979); cipherText.append(99); cipherText.append(447); cipherText.append(1443);
+cipherText.append(3237); cipherText.append(1032); cipherText.append(2382); cipherText.append(871);
+cipherText.append(843); cipherText.append(1655); cipherText.append(99); cipherText.append(871);
+cipherText.append(1443); cipherText.append(99); cipherText.append(4242);
+cipherText.append(843); cipherText.append(99); cipherText.append(4191); cipherText.append(2269 );
+cipherText.append(99); cipherText.append(843); cipherText.append(4191); cipherText.append(2269 );
+cipherText.append(2979); cipherText.append(99); cipherText.append(871); cipherText.append(1443 );
+cipherText.append(99); cipherText.append(2382); cipherText.append(2269); cipherText.append(843); 
+cipherText.append(99); cipherText.append(4191); cipherText.append(2269); cipherText.append(99);
+cipherText.append(99); cipherText.append(3237); cipherText.append(2979); cipherText.append(99); 
+cipherText.append(871); cipherText.append(843); cipherText.append(3780); cipherText.append(843 );
+cipherText.append(1032); cipherText.append(2088); cipherText.append(1443); cipherText.append(2962 );
+cipherText.append(843); cipherText.append(2916); cipherText.append(99); cipherText.append(3237 );
+cipherText.append(2979); cipherText.append(99); cipherText.append(764); cipherText.append(2218 );
+cipherText.append(2269); cipherText.append(2088); cipherText.append(99); cipherText.append(2088 );
+cipherText.append(4191); cipherText.append(2269); cipherText.append(99); cipherText.append(447 );
+cipherText.append(1443); cipherText.append(3237); cipherText.append(843); cipherText.append(99 );
+cipherText.append(871); cipherText.append(1655); cipherText.append(2382); cipherText.append(843 );
+cipherText.append(99); cipherText.append(4242); cipherText.append(843); cipherText.append(447 );
+cipherText.append(4191); cipherText.append(2382); cipherText.append(2269); cipherText.append(843 );
+cipherText.append(99); cipherText.append(2218); cipherText.append(99); cipherText.append(447 );
+cipherText.append(4191); cipherText.append(2962); cipherText.append(99); cipherText.append(2962 );
+cipherText.append(1443); cipherText.append(99); cipherText.append(3780); cipherText.append(1443 );
+cipherText.append(2962); cipherText.append(1294); cipherText.append(843); cipherText.append(1655 );
+cipherText.append(99); cipherText.append(2970); cipherText.append(2218); cipherText.append(1294 );
+cipherText.append(2382); cipherText.append(1655); cipherText.append(843); cipherText.append(99 );
+cipherText.append(1443); cipherText.append(2382); cipherText.append(871); cipherText.append(99 );
+cipherText.append(2088); cipherText.append(1443); cipherText.append(764); cipherText.append(99 );
+cipherText.append(871); cipherText.append(1443); cipherText.append(99); cipherText.append(2382 );
+cipherText.append(2269); cipherText.append(843); cipherText.append(99); cipherText.append(3237 );
+cipherText.append(2979); cipherText.append(99); cipherText.append(871); cipherText.append(843 );
+cipherText.append(3780); cipherText.append(843); cipherText.append(1032); cipherText.append(2088 );
+cipherText.append(1443); cipherText.append(2962); cipherText.append(843); cipherText.append(2916 );
+cipherText.append(1400);
